b52ed642a9884907771009999fc60638b47f8199
crcruzr <cristian.alexander.cruz.rodriguez@umontreal.ca>
2025-06-26 15:43:47 -0400

    I’ve added a validation test to be sure each iteration was correctly generated

--- 01Scripts/SimConstrains_datapreparation.r
+++ 01Scripts/SimConstrains_datapreparation.r
@@ -1,10 +1,8 @@
-
 library(tidyverse)
 library(rlpi)
 library(future)
 library(future.apply)
 library(data.table)
-library(patchwork)
 
 route <- '03OutData/'
 exroute <- '04Plots/'
@@ -56,8 +54,24 @@ species_data_final <- species_data_clean[sample(nrow(species_data_clean), nrow(l
 ### Permutations
 ######
 # NAs permutations
-lpi_data_filteredNA <- lpi_data_filteredNA[, !names(lpi_data_filteredNA) %in% c('Binomial', 'ID')]
-lpi_data_filteredNAPer <- permutationLPI(lpi_data_filteredNA, nperm = 300, shuffle_zeros = FALSE, shuffle_NA = TRUE , years, S)
+
+lpi_data_filteredNA[!maskNA] <- species_data_final[!maskNA]
+identical(is.na(lpi_data_filtered), is.na(lpi_data_filteredNA))
+
+lpi_data_filteredNAPer[[sample(S, 1)]] <- permutationLPI(lpi_data_filteredNA, nperm = 300, shuffle_zeros = FALSE, shuffle_NA = TRUE , years, S)
+
+#### Validation of matrices NA approach
+######################
+sum(is.na(lpi_data_filteredNAPer[[as.numeric(sample((length(lpi_data_filteredNAPer)), 1))]])) ## Number of rows with NA
+rowSums(is.na(lpi_data_filteredNAPer[[as.numeric(sample((length(lpi_data_filteredNAPer)), 1))]])) ## Number of rows with NA per row
+sum(is.na(lpi_data_filtered))
+
+#Validated NA consistency
+identical(sum(is.na(lpi_data_filtered)), sum(is.na(lpi_data_filteredNAPer[[as.numeric(sample((length(lpi_data_filteredNAPer)), 1))]]))  )
+identical(rowSums(is.na(lpi_data_filtered)), rowSums((is.na(lpi_data_filteredNAPer[[as.numeric(sample((length(lpi_data_filteredNAPer)), 1))]]))) )
+
+### Validate if the position of NA are different to the original structurel
+identical(is.na(lpi_data_filtered), is.na( lpi_data_filteredNAPer[[as.numeric(sample((length(lpi_data_filteredNAPer)), 1))]] ))
 
 for (i in 1:length(lpi_data_filteredNAPer)) {
   # Create subdirectory path
@@ -76,9 +90,34 @@ for (i in 1:length(lpi_data_filteredNAPer)) {
 ###########
 ###0s permutations
 #####
-lpi_data_filtered02 <- lpi_data_filtered0[, !names(lpi_data_filtered0) %in% c('Binomial', 'ID')]
-lpi_data_filtered0Per <- permutationLPI(lpi_data_filtered02, nperm = 1500, shuffle_zeros = TRUE, shuffle_NA = FALSE, years = years, S = S)
 
+lpi_data_filtered0[!mask0] <- species_data_final[!mask0]
+
+#### Zero position are identical in both matrices
+identical((ifelse(is.na(lpi_data_filtered0), FALSE, lpi_data_filtered0 == 0)),
+(ifelse(is.na(lpi_data_filtered), FALSE, lpi_data_filtered == 0)))
+
+
+
+lpi_data_filtered0Per <- permutationLPI(lpi_data_filtered0, nperm = 300, shuffle_zeros = TRUE, shuffle_NA = FALSE, years = years, S = S)
+
+#### Validation of matirces Zero approach
+######################
+
+## Same number of Zeros in general
+identical((sum(lpi_data_filtered == 0, na.rm = TRUE)), 
+sum((lpi_data_filtered0Per[[  sample(length(lpi_data_filtered0Per),1) ]]) == 0, na.rm = TRUE))
+## Same number of Zeros per row
+identical((rowSums(lpi_data_filtered == 0, na.rm = TRUE)), 
+rowSums((lpi_data_filtered0Per[[  sample(length(lpi_data_filtered0Per),1) ]]) == 0, na.rm = TRUE))
+
+### Validate if the position of Zeros are different to the original structure
+identical(is.na(lpi_data_filtered), 
+is.na( lpi_data_filtered0Per[[as.numeric(sample((length(lpi_data_filtered0Per)), 1))]] ))
+
+#Validate if matrices are identical
+identical((ifelse(is.na(  lpi_data_filtered0Per[[ sample(length(lpi_data_filtered0Per),1)   ]]   ), FALSE, lpi_data_filtered0Per[[  sample(length(lpi_data_filtered0Per),1)    ]] == 0))     ,
+(ifelse(is.na(lpi_data_filtered), FALSE, lpi_data_filtered == 0)))
 
 
 for (i in 1:length(lpi_data_filtered0Per)) {
