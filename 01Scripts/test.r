db7317b7bed250942f8febfc9df7890e6e30784b
crcruzr <cristian.alexander.cruz.rodriguez@umontreal.ca>
2026-01-12 10:19:47 -0500

    🧹 Stop tracking script and plots and ignore generated plot artifacts

--- 01Scripts/test.r
+++
@@ -1,6 +0,0 @@
-args = commandArgs(trailingOnly=TRUE)
-
-name      <- args[1]                # read first argument as string
-
-# print(paste("Processing with name:'", name, sep = ''))
-seq(name, by = 25, length.out=4)
